a616062091bef932034ef9ebe509859b9e8e3f89
Maria Beatriz <mrbtrznogueira@gmail.com>
2017-06-12 09:59:17 -0300

    Segundo ano
    Clone do Mario

---
+++ Clonizinho/offline.js
@@ -0,0 +1,25 @@
+﻿{
+	"version": 1497271971,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/fundo-sheet0.png",
+		"images/blocomario-sheet0.png",
+		"images/chão-sheet0.png",
+		"images/imagemmario-sheet0.png",
+		"images/imagemmario-sheet1.png",
+		"images/imagemmario-sheet2.png",
+		"images/plataforma-sheet0.png",
+		"images/inimigo1-sheet0.png",
+		"images/mudarlado-sheet0.png",
+		"media/level 1.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
